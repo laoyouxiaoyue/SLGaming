15f2929b2671e632b5d471193b58dd5aa258a022
DaisyGet <1905457445@qq.com>
2026-01-11 12:50:48 +0800

    feat: 引入element-plus，按需加载

--- front/SLGaming/src/App.vue
+++ front/SLGaming/src/App.vue
@@ -5,6 +5,7 @@
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
+    <el-button type="primary">i am button</el-button>
   </p>
 </template>
 
