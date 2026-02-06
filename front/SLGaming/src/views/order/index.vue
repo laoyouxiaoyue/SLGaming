ae5b7c2feb4a39c0d6287a93405e548cc83a4f91
DaisyGet <1905457445@qq.com>
2026-02-06 19:50:02 +0800

    fix(order): 移除未使用的watch导入，优化代码整洁性

--- front/SLGaming/src/views/order/index.vue
+++ front/SLGaming/src/views/order/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, watch } from "vue";
+import { ref, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { useInfoStore } from "@/stores/infoStore";
 
